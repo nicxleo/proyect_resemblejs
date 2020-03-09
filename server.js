fa1ed27953223a67b7253999a0ad739f860f52df
Nicolás Jiménez <nicx92@outlook.com>
2020-03-08 23:19:35 -0500

    Nicolas.J-Aplicacion resemble.

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+function startServer() {
+  const app = express();
+
+  // Handle requests for static files
+  app.use(express.static('public'));
+
+  // Start the server
+  return app.listen('8000', () => {
+    // eslint-disable-next-line no-console
+    console.log('Local DevServer Started on port 8000...');
+  });
+}
+
+startServer();
